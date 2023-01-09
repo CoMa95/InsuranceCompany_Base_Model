71bd0427676924fed932947018e2d128aaebc613
Cosmin Manolescu <89095566+CoMa95@users.noreply.github.com>
2023-01-09 11:28:08 +0200

    Delete script1_id_category.sql

--- script1_id_category.sql
+++
@@ -1,9 +0,0 @@
-/* Script description: create a table with only the policy id (idPolita) and the car category */
-
-create table id_categ
-	as select
-		idPolita, categorie
-		from obiecte;
-        
-alter table id_categ
-	add primary key (idPolita);
